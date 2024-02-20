54cddfb99af2196bd87d61e320f2e1b191f78e49
Shruti <shrutiswift@gmail.com>
2024-02-20 23:15:56 +0530

    Changes Done

---
+++ CodechefChallenge/defence.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int N;
+        int a[1000];
+        scanf("%d", &N);
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &a[i]);
+        }
+        if(a[i] == [10,60])
+
+    }
+    return 0;
+}
